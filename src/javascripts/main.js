c89803a1d228404d040fb771a97c09d171a111f7
NickStepWeber <113000993+NickStepWeber@users.noreply.github.com>
2024-01-28 03:12:24 -0700

    Final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,8 +4,34 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // First: Set up your name
-let std_name = "Your name goes here"
+let std_name = "Nicholas Stephenson"
 document.querySelector('#std_name').innerHTML = `<strong>${std_name}</strong>`
 
 //Then: comes everything else
 // TODO
+import * as d3 from "d3";
+
+let svg1 = d3.select('main')
+    .append('svg')
+    .attr('width', 100)
+    .attr('height', 100)
+    .style('background', '#222')
+    .style("fill", "transparent")
+
+let svg1Circle = svg1.append('circle')
+    .attr('cx', 50)
+    .attr('cy', 50)
+    .attr('r', 4)
+    .style('fill', 'pink')
+    .style('stroke', 'pink')
+
+function animateSvg1Circle()
+{
+    svg1Circle
+        .attr('r', 4)
+        .transition()
+        .duration(2000)
+        .attr('r', 30)
+        .on('end', animateSvg1Circle);
+}
+animateSvg1Circle()
